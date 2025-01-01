4073d7adb37467f3828a56a46a091eda15046db2
super_c4t_2 <yusanso.lab@gmail.com>
2025-01-01 14:48:03 +0900

    result of mistralnemo

---
+++ job/inf_icl_bm25_mistral-nemo_93279.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+#PJM -L rscgrp=cx-share
+#PJM -L elapse=24:00:00
+#PJM -j
+#PJM -S
+
+eval "$(~/miniconda3/bin/conda shell.bash hook)"
+conda activate ME_241211
+python ../src/inf_icl.py --icl_method bm25 --seed 93279 --model_name mistralai/Mistral-Nemo-Instruct-2407
