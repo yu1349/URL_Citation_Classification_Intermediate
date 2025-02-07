08aa759916b49ac3306572b85de18caa7aa61df2
super_c4t_2 <yusanso.lab@gmail.com>
2025-02-07 19:03:45 +0900

    move file

--- job/inf_icl_bm25_mistral-nemo_5374.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-#PJM -L rscgrp=cxgfs-share
-#PJM -L elapse=24:00:00
-#PJM -j
-#PJM -S
-
-eval "$(~/miniconda3/bin/conda shell.bash hook)"
-conda activate ME_241211
-python ../src/inf_icl.py --icl_method bm25 --seed 5374 --model_name mistralai/Mistral-Nemo-Instruct-2407
--- job/inf_icl_encoder_gemma-9b_93279.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-#PJM -L rscgrp=cx-share
-#PJM -L elapse=24:00:00
-#PJM -j
-#PJM -S
-
-eval "$(~/miniconda3/bin/conda shell.bash hook)"
-conda activate ME_241211
-python ../src/inf_icl.py --icl_method encoder --seed 93279 --model_name google/gemma-2-9b-it
